6eb96e63649ccb3bfa224685f363161ee7a6f671
Md Akash <akashnazmul649@gmail.com>
2024-02-09 11:40:25 +0600

    add app in setting & print txt

--- project1/games/views.py
+++ project1/games/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def print(request):
+    return HttpResponse("this is my first django project")
